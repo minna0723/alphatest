1073e81de9f2dc1c1293a6ae52d61d4b9c2ba332
minna0723 <minna0723@naver.com>
2023-02-18 20:34:24 +0900

    start project

---
+++ main.js
@@ -0,0 +1,63 @@
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+$(document).ready(function () {
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+                scrollTop: $($(this).attr('href')).offset().top,
+
+            },
+            500,
+            'linear'
+        );
+
+    });
+
+    $(".faq_main").click(function() {
+        $(this).next(".show-open").stop().slideToggle(300);
+       $(this).toggleClass('on').siblings().removeClass('on');
+       $(this).next(".show-open").siblings(".show-open").slideUp(300); // 1개씩 펼치기
+     });
+
+});
+
+/* rank swiper */
+var swiper = new Swiper(".rankSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 375
+    375: {
+      slidesPerView: 3,
+      spaceBetween: 110,
+    },
+     // when window width is >= 1100
+     1100: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    // when window width is >= 1400
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    },
+    // when window width is >= 1700
+    1700: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    },
+  }
+});
