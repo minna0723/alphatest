feed5e6f41c583be1f940691679ce585eae51352
minna0723 <minna0723@naver.com>
2023-02-20 23:05:53 +0900

    top menu btn preventdefault

--- common/common.js
+++ common/common.js
@@ -4,3 +4,7 @@ const menu = document.querySelector('.navbar_menu');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+});
