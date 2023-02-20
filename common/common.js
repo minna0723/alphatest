cd81a246fa4fdf7734c599ecf019fe874eaf5640
minna0723 <minna0723@naver.com>
2023-02-20 23:12:34 +0900

    togglebtn e preventedefault

--- common/common.js
+++ common/common.js
@@ -1,10 +1,7 @@
 const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
-
-$('a[href*="#"]').on('click', function (e) {
+toggleBtn.addEventListener('click', e => {
     e.preventDefault();
+    menu.classList.toggle('active');
 });
