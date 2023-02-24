36fdb3bd3ebb08e8c7e6f358f5039080c1825bc3
hyemin <minna0723@naver.com>
2023-02-24 22:57:30 +0900

    scroll aboutBTN

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 
         $('html, body').animate({
 
-                scrollTop: $($(this).attr('href')).offset().top,
+                scrollTop: $($(this).attr('href')).offset().top-100,
 
             },
             500,
