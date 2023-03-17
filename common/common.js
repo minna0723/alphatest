42f740dc65c92edbaf64cf6b41ab513d9219eee6
hyemin <minna0723@naver.com>
2023-03-18 02:41:19 +0900

    link copy btn

--- common/common.js
+++ common/common.js
@@ -8,3 +8,15 @@ toggleBtn.addEventListener('click', e => {
     document.querySelector('.navbar_toggleBtn i').classList.toggle('fa-solid');
     document.querySelector('.navbar_toggleBtn i').classList.toggle('fa-bars');
 });
+
+function link_copy(){
+	var url = '';
+	var textarea = document.createElement("textarea");
+	document.body.appendChild(textarea);
+	url = window.document.location.href;
+	textarea.value = url;
+	textarea.select();
+	document.execCommand("copy");
+	document.body.removeChild(textarea);
+	alert("링크 주소가 복사되었습니다.")
+}
